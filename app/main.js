52495112fe582c704a87930d4f1ca35936b87d0d
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-06-11 22:38:33 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,7 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const httVersion = "HTTP/1.1";
+    const statusCode = "200";
+    const reasonPhrase = "OK";
+    const crlf = "\r\n\r\n";
+
+    socket.write(`${httVersion} ${statusCode} ${reasonPhrase}${crlf}`);
     socket.on("close", () => {
         socket.end();
     });
